5798be2f3bd96918ee414f34d16dd43ae2c1ba7d
Harris Salimo <harrismailasalimo@gmail.com>
2025-05-08 21:42:51 +0300

    refactor: streamline nginx configuration and update deployment script for caching

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -1,13 +1,19 @@
 #!/usr/bin/env bash
 echo "Running composer"
-composer global require hirak/prestissimo
+# composer global require hirak/prestissimo
 composer install --no-dev --working-dir=/var/www/html
 
 echo "Caching config..."
 php artisan config:cache
 
-echo "Caching routes..."
+echo "Caching event..."
+php artisan event:cache
+
+echo "Caching route..."
 php artisan route:cache
+
+echo "Caching view..."
+php artisan view:cache
 
 echo "Running migrations..."
 php artisan migrate --force
